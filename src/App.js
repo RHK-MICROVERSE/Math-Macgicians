38d2cebb43b2b23abd6bc0129084c01ccddd1885
Rassel Hassan Kadir <9rhktrading@gmail.com>
2023-05-02 16:35:41 +0600

    Update Home Calculator and Quote page and style implementation

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Calculator from './components/Calculator';
 import Quote from './components/quotes';
+import Nav from './components/nav';
+import Home from './components/homePage';
 
 function App() {
   return (
-    <div>
-      <Calculator />
-      <Quote />
-    </div>
+    <Router>
+      <div>
+        <Nav/>
+          <div className='pageContainer'>
+            <Routes>
+              <Route path='/' element={<Home/>}/>
+              <Route path='/calculator' element={<Calculator/>}/>
+              <Route path='/quotes' element={<Quote/>}/>
+            </Routes>
+        </div>
+      </div>
+    </Router>
   );
 }
 
